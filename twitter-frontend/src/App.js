f4c47f9f523de6844723365a070a38387e75b171
Safimmi <asztangie2@gmail.com>
2022-03-18 16:48:29 -0500

    Set up

---
+++ twitter-frontend/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello From React</h1>
+      <Header title = 'Hello'/>
+    </div>
+  );
+}
+
+export default App;
